ca176d92d2589c8602ed36d1e7c3cadd8b6744f9
AIA <adashik585@gmail.com>
2022-04-25 13:12:03 +0600

    genius car services server

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6lujg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('geniusCar').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        // POST
+        app.post('/service', async (req, res) => {
+            const nweService = req.body;
+            const result = await serviceCollection.insertOne(nweService);
+            res.send(result);
+        });
+
+        // DELETE
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+const port = process.env.POTR || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Genius car service Server');
+});
+
+app.listen(port, () => {
+    console.log('server is running');
+})
